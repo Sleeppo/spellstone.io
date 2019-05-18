7f43820dcf1b2c53ccdb1f684ebba6099d2b5298
Jacob Wilde <jwSleepo@gmail.com>
2019-05-18 16:36:23 +0200

    ES6 module test

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,4 @@
-console.log('test');
+import Player from './player';
+
+let player1 = new Player({name: 'Talasi'});
+console.log(player1.name);
---
+++ javascripts/player.js
@@ -0,0 +1,5 @@
+export default class Player {
+  constructor(player) {
+    this.name = player.name;
+  }
+}
